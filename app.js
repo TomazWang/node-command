2075af8914bf4466b72d3decb9086d226ff0c264
Tomaz Wang <TomazWang.tw@gmail.com>
2018-07-01 14:31:21 +0800

    init backup

---
+++ app.js
@@ -0,0 +1,9 @@
+const test = JSON.parse('{"type":"service_account","project_id":"tomaz-bot","private_key_id":"4d3cf3f707829c37af015965f3047cfea7253f50","private_key":"-----BEGIN PRIVATE KEY-----","client_email":"firebase-adminsdk-2fakt@tomaz-bot.iam.gserviceaccount.com","client_id":"102130386750361336808","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2fakt%40tomaz-bot.iam.gserviceaccount.com"}');
+
+
+console.log(`typeof test ${typeof test}`);
+console.log(test);
+
+
+console.log(test.type)
+console.log(test.project_id)
